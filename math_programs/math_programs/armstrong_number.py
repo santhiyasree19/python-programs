3c699ca34de892c55996d26fbb09f7a3f27aff77
santhiyasree19 <sakthilakshmi4345@gmail.com>
2026-06-17 23:10:33 +0530

    feat: add armstrong number checker

---
+++ math_programs/math_programs/armstrong_number.py
@@ -0,0 +1,18 @@
+n=int(input())
+def count(n):
+    c=0
+    while(n):
+        n=n//10
+        c=c+1
+    return(c)
+x=n
+s=0
+c=count(n)
+while(n):
+    a=n%10
+    s=s+a**c
+    n=n//10
+if(x==s):
+    print("armstrong")
+else:
+    print("not a armstrong")
